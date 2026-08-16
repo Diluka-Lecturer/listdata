714b65d71e8e0fb7e7b6002bb90825f7c130097b
diluka <dt.gpt85@gmail.com>
2026-08-16 10:32:19 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+// Note: Adjust the import path if your project name is different
+import 'package:datamodel_listdata/model/book_details.dart'; 
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: BookListScreen(),
+  ));
+}
+
+class BookListScreen extends StatefulWidget {
+  const BookListScreen({super.key});
+
+  @override
+  State<BookListScreen> createState() => _BookListScreenState();
+}
+
+class _BookListScreenState extends State<BookListScreen> {
+  
+  // TODO 4: Create a List of BookDetails objects and add at least 3 books to it.
+  // Example: BookDetails(title: "THE FROZEN RIVER", author: "Ariel Lawhon")
+  List<BookDetails> books = [
+    
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Book App"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                // TODO 5: Set the itemCount to the total number of items in your 'books' list
+                itemCount: 0, 
+                
+                itemBuilder: (context, index) {
+                  // TODO 6: Return a ListTile widget to display the book data
+                  // - Set the 'title' parameter using a Text widget: Text(books[index].title)
+                  // - Set the 'trailing' parameter using a Text widget for the author
+                  // - Add an 'onTap' parameter with an anonymous function that prints the title to the console
+                  return Container(); 
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/model/book_details.dart
@@ -0,0 +1,8 @@
+// TODO 1: Create a class named BookDetails
+class BookDetails {
+  // TODO 2: Declare two String variables: 'title' and 'author'
+  
+  
+  // TODO 3: Create a constructor that requires 'title' and 'author' to be passed in
+  
+}
